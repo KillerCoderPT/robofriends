e8332e2bd4491a850fe95276ca88e02b9b2a8f08
KillerCoder <54954227+KillerCoderPT@users.noreply.github.com>
2020-12-26 18:01:14 +0000

    Add ErrorBoundry to the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
 import Footer from '../components/Footer';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
@@ -39,7 +40,9 @@ class App extends Component {
                                                                 <h1 className='f1'>RoboFriends</h1>
                                                                 <SearchBox searchChange={this.onSearchChange} />
                                                                 <Scroll>
-                                                                    <CardList robots={filteredRobots} />
+                                                                    <ErrorBoundry>
+                                                                        <CardList robots={filteredRobots} />
+                                                                    </ErrorBoundry>
                                                                 </Scroll>
                                                                 <Footer />
                                                             </div>);
